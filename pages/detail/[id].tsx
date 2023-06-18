1b2390b6276286060568eb8553db5405d0f8f42e
lastorder <adzkiyadzikrullah@gmail.com>
2023-06-18 18:32:41 +0700

    detail update - 1

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -39,7 +39,7 @@ export default function Detail({ dataItem, nominals, payments }: DetailProps) {
             <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
               <TopUpItem data={dataItem} type="desktop" />
               <hr />
-              {/* <TopUpForm nominals={nominals} payments={payments} /> */}
+              <TopUpForm nominals={nominals} payments={payments} />
             </div>
           </div>
         </div>
